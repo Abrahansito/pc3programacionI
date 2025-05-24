ba621352dfde955c5369c4df0d67ccb0ee3c111d
Abrahansito <huamancarlosabraham@gmail.com>
2025-05-23 21:41:51 -0500

    API local de feedback con validación

---
+++ Models/Feedback.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace startup.Models
+{
+    public class Feedback
+    {
+        
+        public int Id { get; set; } 
+        public int PostId { get; set; }
+        public string? Sentimiento { get; set; }
+        public DateTime Fecha { get; set; }
+    }
+}
